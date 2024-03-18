42d30eabcaf82f87671cbbbfb2eb57febb5a1b90
ishitsethi <ishit.sethi@ignitarium.com>
2024-03-19 00:01:38 +0530

    added console for checking

--- Backend/index.js
+++ Backend/index.js
@@ -1,12 +1,13 @@
-require('dotenv').config();
-const express = require('express')
+require("dotenv").config();
+const express = require("express");
 const app = express();
 // const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('hello world')
+app.get("/", (req, res) => {
+  res.send("hello world");
 });
 
+console.log("hiii this is ishit");
 app.listen(process.env.PORT || 5000, () => {
-    console.log(`Provacc running on ${process.env.PORT || 5000}`)
-})
+  console.log(`Provacc running on ${process.env.PORT || 5000}`);
+});
